42a40ff8395f89add3e0c09d1807f738ca9cb8e0
Setsugennoao <41454651+Setsugennoao@users.noreply.github.com>
2022-04-14 21:02:55 +0200

    Update gyp-find-sdk.js

--- scripts/gyp-find-sdk.js
+++ scripts/gyp-find-sdk.js
@@ -1,2 +1,22 @@
-// reg query "HKEY_CURRENT_USER\\SOFTWARE\\VapourSynth" /v Path
-console.log('C:\\Users\\Matteo\\AppData\\Local\\Programs\\VapourSynth\\sdk')
+const { execSync } = require('child_process')
+
+let regKey = null
+
+try {
+    regKey = execSync('reg query "HKEY_CURRENT_USER\\SOFTWARE\\VapourSynth" /v Path', { encoding: 'utf-8'}).trim()
+} catch {}
+
+
+if (!regKey) {
+    try {
+        regKey = execSync('reg query "HKEY_LOCAL_MACHINE\\SOFTWARE\\VapourSynth" /v Path', { encoding: 'utf-8'}).trim()
+    } catch {}
+}
+
+if (!regKey) process.exit(1)
+
+const installationPath = regKey.split('\n')[1].split(' '.repeat(4))[3]
+
+console.log(`${installationPath}\\sdk`)
+
+// TODO support Linux and portable installations
